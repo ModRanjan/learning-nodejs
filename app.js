c88cb84a1221ce236a84a735111edaa4e0bcba52
ranjan.m <modranjan1999@gmail.com>
2022-05-27 15:52:39 +0530

    adding-middleware

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+// adding middlewares
+app.use((req, res, next) => {
+  console.log('In the middleware!')
+  next() // Allows the request to continue to the next middleware in line
+})
+
+app.use((req, res, next) => {
+  console.log('In another middleware!')
+  res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000, () => {
+  console.log('listening at port 3000')
+})
