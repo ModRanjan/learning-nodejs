5a5f205e015a0d9c45a2d0ba3f601f43cc12eea1
ranjan.m <modranjan1999@gmail.com>
2022-05-27 21:51:19 +0530

    02-05-working-on-layout-with-partials(ejs templeting)

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+// set view engine to ejs
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
