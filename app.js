99ec820a2854612c8a605328c08b1bd88e60450c
ranjan.m <modranjan1999@gmail.com>
2022-05-27 16:12:36 +0530

    04-using-express-router

--- app.js
+++ app.js
@@ -3,28 +3,12 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    `<form action="/product" method="POST">
-    title:
-    <input type="text" name="title">
-    price:
-    <input type="text" name="price">
-    <button type="submit">Add Product</button></form>`
-  )
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>')
-})
-
-app.listen(3000, () => {
-  console.log('listening to port 3000')
-})
+app.listen(3000)
