702605bcc8e33e8aed3c54ddd8e02cdd30a1393e
ranjan.m <modranjan1999@gmail.com>
2022-05-27 16:20:45 +0530

    05-filtering-paths

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.listen(3000)
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
+
+app.listen(3000, () => {
+  console.log(`listening to port 3000`)
+})
