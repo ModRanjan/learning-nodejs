ab38139bdcbb852392fe8ed248df4e8340367c70
ranjan.m <modranjan1999@gmail.com>
2022-05-28 20:23:22 +0530

    03-fetching-data-from-file (models -> product.js) (still getting error. lets figure-out in next commit)

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
-// here we define the shape of product
-const products = []
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class Product {
   constructor(t) {
@@ -7,10 +7,34 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    )
+    fs.readFile(p, (err, fileContent) => {
+      let products = []
+      if (!err) {
+        products = JSON.parse(fileContent)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    )
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return []
+      }
+      return JSON.parse(fileContent)
+    })
   }
 }
