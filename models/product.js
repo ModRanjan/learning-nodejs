47e7f7b686ea33a904ee74572784e91484ecbb0e
ranjan.m <modranjan1999@gmail.com>
2022-05-28 19:55:50 +0530

    02-finished-the-models (product)

---
+++ models/product.js
@@ -0,0 +1,16 @@
+// here we define the shape of product
+const products = []
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t
+  }
+
+  save() {
+    products.push(this)
+  }
+
+  static fetchAll() {
+    return products
+  }
+}
