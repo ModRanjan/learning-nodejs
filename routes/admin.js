ccb26a2b7f7465039101c85b51e42d6c272d005e
ranjan.m <modranjan1999@gmail.com>
2022-05-28 19:42:11 +0530

    01-finished-the-controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,15 @@
 const path = require('path')
+
 const express = require('express')
 
-const rootDir = require('../util/path')
+const productsController = require('../controllers/products')
 
 const router = express.Router()
 
-const products = []
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct)
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    fromsCSS: true,
-    productsCSS: true,
-    activeAddProduct: true,
-  })
-})
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct)
 
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
-  products.push({ title: req.body.title })
-  res.redirect('/')
-})
-
-exports.routes = router
-exports.products = products
+module.exports = router
