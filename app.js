d842c7660e129bc42464958f4ac8bccb67f29560
ranjan.m <modranjan1999@gmail.com>
2022-06-01 12:51:19 +0530

    01-connected-to-database(name: node-complete)-&-fetch/show-products

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const errorController = require('./controllers/error')
 
+// for testing purpose (database)
+const db = require('./util/database')
+
 const app = express()
 
 // set view engine to ejs
@@ -11,6 +14,15 @@ app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
+// // for testing purpose (database)
+// db.execute('SELECT * FROM products')
+//   .then((result) => {
+//     console.log(result[0])
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
