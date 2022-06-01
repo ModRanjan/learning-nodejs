c021d2153e60409500a1e9aa5ab97336a14efce8
ranjan.m <modranjan1999@gmail.com>
2022-06-01 18:54:21 +0530

    04-getting-a-single-product-from-products)

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,28 +3,39 @@ const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
-  Product.findAll()
     .then((products) => {
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/',
+        pageTitle: 'All Products',
+        path: '/products',
       })
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findAll({ where: { id: prodId } })
+    .then((products) => {
       res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
+        product: products[0],
+        pageTitle: products[0].title,
         path: '/products',
       })
     })
     .catch((err) => console.log(err))
+  // // ALTERNATIVE WAY
+  // Product.findById(prodId)
+  //   .then((product) => {
+  //     res.render('shop/product-detail', {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: '/products',
+  //     })
+  //   })
+  //   .catch((err) => console.log(err))
 }
 
 exports.getIndex = (req, res, next) => {
@@ -36,7 +47,9 @@ exports.getIndex = (req, res, next) => {
         path: '/',
       })
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 exports.getCart = (req, res, next) => {
