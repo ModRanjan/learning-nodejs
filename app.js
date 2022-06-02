2eba7c5352d6e5f59cb96b4b5b64a5a53b61c2ff
ranjan.m <modranjan1999@gmail.com>
2022-06-02 19:21:50 +0530

    11-adding-One-To-Many-relation (added-sequelize-association) (for-Cart-&-CartItem)

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
-const sequelize = require('./util/database')
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
-const Product = require('./models/product')
+const Product = require('./models/product');
 const User = require('./models/user')
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next)=>{
   User.findByPk(1)
@@ -27,17 +29,23 @@ app.use((req,res,next)=>{
     next();
   })
   .catch(err=>console.log(err));
-})
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  .sync({force:true})
+  // .sync({force:true})
+  .sync()
   .then((result) => {
     // console.log(result)
     return User.findByPk(1);
