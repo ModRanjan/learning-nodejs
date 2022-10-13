8010bad829b13f8244963c05eb5db917834c71b7
Tushar Mahajan <tushar.m@simformsolutions.com>
2022-10-13 12:53:29 +0530

    add-new-compiler-version

--- hardhat.config.js
+++ hardhat.config.js
@@ -75,7 +75,7 @@ module.exports = {
     },
   },
   solidity: {
-    compilers: [{ version: '0.8.9' }],
+    compilers: [{ version: '0.8.9' }, { version: '0.8.8' }],
     settings: {
       optimizer: {
         enabled: true,
